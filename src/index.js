b74a398f45ad0e81054210c9c57ed91dbedd7936
Tomasz Goralczyk <devflag@gmail.com>
2017-06-14 13:48:49 -0400

    Installed more dependencies and created first files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+// import { createStore, applyMiddleware } from "redux";
+// import { Provider } from "react-redux";
+// import thunk from "redux-thunk";
+import App from "./containers/App";
+
+render(<App />, document.getElementById("root"));
