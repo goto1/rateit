58cd50b3541d80418ef703d1dc4600867a3124f2
Tomasz Goralczyk <devflag@gmail.com>
2017-06-22 06:38:30 -0400

    Deleted unnecessary code

--- src/index.js
+++ src/index.js
@@ -5,14 +5,6 @@ import store from "./store";
 import App from "./containers/App";
 import "./index.css";
 
-// import { navigateTo } from "framework7-redux";
-// import { routeChange } from "./actions";
-
-// console.log(navigateTo);
-
-// store.dispatch(navigateTo("/search/", true));
-// store.dispatch(routeChange("/search/"));
-
 render(
   <Provider store={store}>
     <App />
