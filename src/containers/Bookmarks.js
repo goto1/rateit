4fd49fbf8ac301f7308ad0948ab302dc37b97745
Tomasz Goralczyk <devflag@gmail.com>
2017-06-23 10:49:33 -0400

    Added proptypes and made small changes

--- src/containers/Bookmarks.js
+++ src/containers/Bookmarks.js
@@ -1,38 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 import people from "../dummy-data"; // To Be Deleted...
-import PersonListItem from "../components/PersonListItem";
+import PersonOverviewCard from "../components/PersonOverviewCard";
 import {
   Page,
   Navbar,
   Subnavbar,
   ButtonsSegmented,
   Button,
-  Buttons,
   Tabs,
   Tab,
   ContentBlock,
-  ContentBlockTitle,
-  List
+  ContentBlockTitle
 } from "framework7-react";
 
-const ContentBlockCustom = ({ children, title }) =>
+const ContentBlockCustom = ({ title, children }) =>
   <ContentBlock style={{ padding: "0" }}>
     <ContentBlockTitle>{title.toUpperCase()}</ContentBlockTitle>
     {children}
   </ContentBlock>;
 
+ContentBlockCustom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.array.isRequired
+};
+
 export const SavedProfessors = () =>
   <ContentBlockCustom title="Your saved professors">
-    <List inset>
-      {people.map(person => <PersonListItem key={person.id} {...person} />)}
-    </List>
+    {people.map(person => <PersonOverviewCard key={person.id} {...person} />)}
   </ContentBlockCustom>;
 
 export const SavedStudents = () =>
   <ContentBlockCustom title="Your saved students">
-    <List inset>
-      {people.map(person => <PersonListItem key={person.id} {...person} />)}
-    </List>
+    {people.map(person => <PersonOverviewCard key={person.id} {...person} />)}
   </ContentBlockCustom>;
 
 const Bookmarks = () =>
