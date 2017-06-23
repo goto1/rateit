c1bbb4704dc13d981c9e606589a5f2df6f089f58
Tomasz Goralczyk <devflag@gmail.com>
2017-06-23 10:15:14 -0400

    Formatting changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,11 @@ import MainLayout from "../layouts/MainLayout";
 import F7App from "./F7App";
 import Search from "../containers/Search";
 
-// prettier-ignore
-const App = () => (
+const App = () =>
   <MainLayout>
     <F7App>
       <Search />
     </F7App>
-  </MainLayout>
-)
+  </MainLayout>;
 
 export default App;
