b4dd777a8adf7b34b37a6c4e5bafa44d29a7d323
Tomasz Goralczyk <devflag@gmail.com>
2017-06-24 12:44:21 -0400

    Added 'ratings' attribute to each person

--- src/dummy-data.js
+++ src/dummy-data.js
@@ -5,7 +5,33 @@ const people = [
     type: "Student",
     school: "NJIT",
     rating: 4.5,
-    numOfRatings: 201
+    numOfRatings: 201,
+    ratings: [
+      {
+        description: "Attends Group Meetings",
+        rating: 3
+      },
+      {
+        description: "Contributes to Discussions",
+        rating: 5
+      },
+      {
+        description: "Completes Tasks on Time",
+        rating: 4
+      },
+      {
+        description: "Quality of Completed Work",
+        rating: 2
+      },
+      {
+        description: "Cooperative & Supportive",
+        rating: 3
+      },
+      {
+        description: "Contributes Significantly",
+        rating: 3
+      }
+    ]
   },
   {
     id: "7456062367375738",
@@ -13,7 +39,29 @@ const people = [
     type: "Professor",
     school: "NJIT",
     rating: 3,
-    numOfRatings: 110
+    numOfRatings: 110,
+    ratings: [
+      {
+        description: "Ability to Communicate Course Content",
+        rating: 3
+      },
+      {
+        description: "Availability Outside of Class Hours",
+        rating: 1
+      },
+      {
+        description: "Fairness & Consistency in Grading",
+        rating: 4
+      },
+      {
+        description: "Knowledge of Course Material",
+        rating: 3
+      },
+      {
+        description: "Overall Teaching Effectivness",
+        rating: 3
+      }
+    ]
   },
   {
     id: "9766491935583002",
@@ -21,7 +69,33 @@ const people = [
     type: "Student",
     school: "Rutgers",
     rating: 2.5,
-    numOfRatings: 30
+    numOfRatings: 30,
+    ratings: [
+      {
+        description: "Attends Group Meetings",
+        rating: 3
+      },
+      {
+        description: "Contributes to Discussions",
+        rating: 5
+      },
+      {
+        description: "Completes Tasks on Time",
+        rating: 2
+      },
+      {
+        description: "Quality of Completed Work",
+        rating: 3
+      },
+      {
+        description: "Cooperative & Supportive",
+        rating: 3
+      },
+      {
+        description: "Contributes Significantly",
+        rating: 3
+      }
+    ]
   },
   {
     id: "6428626191061508",
@@ -29,7 +103,33 @@ const people = [
     type: "Professor",
     school: "Rutgers",
     rating: 5,
-    numOfRatings: 63
+    numOfRatings: 63,
+    ratings: [
+      {
+        description: "Attends Group Meetings",
+        rating: 3
+      },
+      {
+        description: "Contributes to Discussions",
+        rating: 5
+      },
+      {
+        description: "Completes Tasks on Time",
+        rating: 4
+      },
+      {
+        description: "Quality of Completed Work",
+        rating: 3
+      },
+      {
+        description: "Cooperative & Supportive",
+        rating: 3
+      },
+      {
+        description: "Contributes Significantly",
+        rating: 3
+      }
+    ]
   }
 ];
 
