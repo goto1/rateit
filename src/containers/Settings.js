fd7f004a0bf2a43829f6f434c3ddc81fbf0a7ced
Tomasz Goralczyk <devflag@gmail.com>
2017-06-25 07:53:52 -0400

    Moved Settings component into a separate file

---
+++ src/containers/Settings.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Page, Navbar, ContentBlock } from "framework7-react";
+
+// prettier-ignore
+const Settings = () => (
+  <Page>
+    <Navbar title='Settings' sliding />
+    <ContentBlock inner>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quis est illum ex. Officiis nemo sapiente velit quo itaque recusandae soluta nesciunt a magnam ab enim vitae perferendis, repudiandae minima.
+    </ContentBlock>
+  </Page>
+)
+
+export default Settings;
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,10 @@
 import React from "react";
 import Search from "./containers/Search";
+import Settings from "./containers/Settings";
 import Bookmarks, {
   SavedProfessors,
   SavedStudents
 } from "./containers/Bookmarks";
-import { Page, Navbar, ContentBlock } from "framework7-react";
-
-const Settings = () =>
-  <Page>
-    <Navbar title="Settings" backLink="Back" sliding />
-    <ContentBlock inner>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
-      laboriosam voluptate facilis libero sint voluptates reiciendis
-      necessitatibus quis nostrum quidem, autem mollitia voluptatibus iure
-      dolores, incidunt deserunt, omnis esse. Reiciendis.
-    </ContentBlock>
-  </Page>;
 
 const routes = [
   {
