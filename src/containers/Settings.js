28275658e437d8aaa404561c79d00cbe21e11a87
Tomasz Goralczyk <devflag@gmail.com>
2017-06-25 08:52:07 -0400

    Implemented lists with options for the user to take advantage of

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,15 +1,74 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Page, Navbar, ContentBlock } from "framework7-react";
+import ContentBlockTitleCustom from "../layouts/ContentBlockTitleCustom";
+import {
+  Page,
+  Navbar,
+  ContentBlock,
+  List,
+  ListItem,
+  Icon
+} from "framework7-react";
 
-// prettier-ignore
-const Settings = () => (
+const ListItemCustom = ({ icon, link, text }) =>
+  <li>
+    <a href={link} className="item-link item-content">
+      <div className="item-media">
+        <Icon material={icon} />
+      </div>
+      <div className="item-inner">{text}</div>
+    </a>
+  </li>;
+
+ListItemCustom.propTypes = {
+  icon: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
+
+const AccountSettings = () =>
+  <div>
+    <ContentBlockTitleCustom>Account Settings</ContentBlockTitleCustom>
+    <List inset>
+      <ListItemCustom
+        icon="account_circle"
+        link="/settings/general"
+        text="General"
+      />
+    </List>
+  </div>;
+
+const UserSettings = () =>
+  <div>
+    <List inset>
+      <ListItemCustom
+        icon="sentiment_very_satisfied"
+        link="/profile/id/"
+        text="Profile Page"
+      />
+      <ListItemCustom icon="exit_to_app" link="/signout/" text="Sign Out" />
+    </List>
+  </div>;
+
+const HelpAndSupport = () =>
+  <div>
+    <ContentBlockTitleCustom>Help and Support</ContentBlockTitleCustom>
+    <List inset>
+      <ListItemCustom icon="help" link="/settings/help/" text="Help Center" />
+      <ListItemCustom
+        icon="assignment"
+        link="/settings/terms/"
+        text="Terms & Policies"
+      />
+    </List>
+  </div>;
+
+const Settings = () =>
   <Page>
-    <Navbar title='Settings' sliding />
-    <ContentBlock inner>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum quis est illum ex. Officiis nemo sapiente velit quo itaque recusandae soluta nesciunt a magnam ab enim vitae perferendis, repudiandae minima.
-    </ContentBlock>
-  </Page>
-)
+    <Navbar title="Settings" sliding />
+    <AccountSettings />
+    <HelpAndSupport />
+    <UserSettings />
+  </Page>;
 
 export default Settings;
