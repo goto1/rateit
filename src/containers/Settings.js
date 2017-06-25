31c97e7a8f1aacd411fb157b9571bf25f31119cf
Tomasz Goralczyk <devflag@gmail.com>
2017-06-25 11:05:52 -0400

    Moved the ListItemCustom component into a separate file for reusability purposes

--- src/containers/Settings.js
+++ src/containers/Settings.js
@@ -1,36 +1,13 @@
 import React from "react";
-import PropTypes from "prop-types";
-import ContentBlockTitleCustom from "../layouts/ContentBlockTitleCustom";
-import {
-  Page,
-  Navbar,
-  ContentBlock,
-  List,
-  ListItem,
-  Icon
-} from "framework7-react";
-
-const ListItemCustom = ({ icon, link, text }) =>
-  <li>
-    <a href={link} className="item-link item-content">
-      <div className="item-media">
-        <Icon material={icon} />
-      </div>
-      <div className="item-inner">{text}</div>
-    </a>
-  </li>;
-
-ListItemCustom.propTypes = {
-  icon: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired
-};
+import ContentBlockTitleCustom from "../components/f7/ContentBlockTitleCustom";
+import { ListItemLinkCustom } from "../components/f7/ListItems";
+import { Page, Navbar, List } from "framework7-react";
 
 const AccountSettings = () =>
   <div>
-    <ContentBlockTitleCustom>Account Settings</ContentBlockTitleCustom>
+    <ContentBlockTitleCustom text="Account settings" />
     <List inset>
-      <ListItemCustom
+      <ListItemLinkCustom
         icon="account_circle"
         link="/settings/general"
         text="General"
@@ -38,28 +15,32 @@ const AccountSettings = () =>
     </List>
   </div>;
 
-const UserSettings = () =>
-  <div>
-    <List inset>
-      <ListItemCustom
-        icon="sentiment_very_satisfied"
-        link="/profile/id/"
-        text="Profile Page"
-      />
-      <ListItemCustom icon="exit_to_app" link="/signout/" text="Sign Out" />
-    </List>
-  </div>;
-
 const HelpAndSupport = () =>
   <div>
-    <ContentBlockTitleCustom>Help and Support</ContentBlockTitleCustom>
+    <ContentBlockTitleCustom text="Help and support" />
     <List inset>
-      <ListItemCustom icon="help" link="/settings/help/" text="Help Center" />
-      <ListItemCustom
+      <ListItemLinkCustom
+        icon="help"
+        link="/settings/help/"
+        text="Help Center"
+      />
+      <ListItemLinkCustom
         icon="assignment"
         link="/settings/terms/"
         text="Terms & Policies"
       />
+    </List>
+  </div>;
+
+const UserSettings = () =>
+  <div>
+    <List inset>
+      <ListItemLinkCustom
+        icon="sentiment_very_satisfied"
+        link="/profile/id/"
+        text="Profile Page"
+      />
+      <ListItemLinkCustom icon="exit_to_app" link="/signout/" text="Sign Out" />
     </List>
   </div>;
 
