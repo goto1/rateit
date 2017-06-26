f62f4386170a6d7b8699846458b46a306f3767a5
Tomasz Goralczyk <devflag@gmail.com>
2017-06-26 12:00:22 -0400

    Created more custom components for the General container

--- src/components/f7/index.js
+++ src/components/f7/index.js
@@ -1,4 +1,13 @@
 import ListItemCustom from "./ListItemCustom";
+import ListBlockCustom from "./ListBlockCustom";
 import ContentBlockTitleCustom from "./ContentBlockTitleCustom";
+import FormInputCustom from "./FormInputCustom";
+import SmartSelectCustom from "./SmartSelectCustom";
 
-export { ListItemCustom, ContentBlockTitleCustom };
+export {
+  ListItemCustom,
+  ListBlockCustom,
+  ContentBlockTitleCustom,
+  FormInputCustom,
+  SmartSelectCustom
+};
