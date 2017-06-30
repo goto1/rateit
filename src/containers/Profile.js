078aa5bb8e7fcb27040c84d7b81e091ce097ba24
Tomasz Goralczyk <devflag@gmail.com>
2017-06-30 11:15:27 -0400

    Finished up styling the card for the Student Details container

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import HorizontalRule from "../components/HorizontalRule";
 import {
   Page,
   Navbar,
@@ -9,56 +11,91 @@ import {
   Tabs,
   Tab,
   Card,
-  CardHeader,
   CardContent,
-  CardFooter
+  CardFooter,
+  Icon
 } from "framework7-react";
 
-const CardCustom = styled(Card)`
-  margin-top: 10px;
-  margin-bottom: 35px;
-`;
+const CardCustom = ({ children }) =>
+  <Card style={{ marginTop: "25px", marginBottom: "35px" }}>{children}</Card>;
 
-const CardHeaderCustom = ({ name, rating }) => {
-  let ratingColor = "#A1BA37";
-
+const NumericRating = ({ rating, fontSize }) => {
+  let color = "#A1BA37";
   if (rating <= 3) {
-    ratingColor = "#DEB21C";
+    color = "#DEB21C";
   }
   if (rating <= 2) {
-    ratingColor = "#D91948";
+    color = "#D91948";
   }
 
   return (
-    <div className="card-header">
-      <div>{name}</div>
-      <div>
-        <span
-          style={{
-            color: ratingColor
-          }}
-        >
-          {rating}
-        </span>/5
-      </div>
+    <div style={{ letterSpacing: "2px" }}>
+      <span style={{ color: color, fontSize: fontSize, fontWeight: "500" }}>
+        {rating}
+      </span>/5
     </div>
   );
 };
 
-export const Ratings = () =>
-  <CardCustom>
-    <CardHeaderCustom name="Randal Rivera" rating={4.5} />
-    <CardContent>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam fuga
-      commodi eligendi a distinctio dicta quaerat quis explicabo reprehenderit,
-      facilis soluta harum, provident, quidem laudantium nesciunt, sint aliquam
-      voluptate non.
-    </CardContent>
-  </CardCustom>;
+NumericRating.propTypes = {
+  rating: PropTypes.number.isRequired,
+  fontSize: PropTypes.string.isRequired
+};
+
+const CardHeaderCustom = ({ title, children }) =>
+  <div className="card-header">
+    <div
+      style={{
+        paddingTop: "4px",
+        textTransform: "uppercase",
+        fontWeight: "500"
+      }}
+    >
+      {title}
+    </div>
+    {children !== undefined && children}
+  </div>;
+
+CardHeaderCustom.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.object
+};
+
+const CardContentItem = ({ icon, text }) =>
+  <div style={{ marginBottom: "5px" }}>
+    <div style={{ display: "flex" }}>
+      <div style={{ marginRight: "10px" }}><Icon material={icon} /></div>
+      <div style={{ paddingTop: "3px" }}>{text}</div>
+    </div>
+    <HorizontalRule
+      width="80%"
+      margin="0 auto"
+      colorOne="#FFFFFF"
+      colorTwo="#747475"
+    />
+  </div>;
+
+CardContentItem.propTypes = {
+  icon: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired
+};
 
 export const Overview = () =>
   <CardCustom>
-    <CardHeaderCustom name="Randal Rivera" rating={4.5} />
+    <CardHeaderCustom title="Randal Rivera">
+      <NumericRating rating={4.5} fontSize="20px" />
+    </CardHeaderCustom>
+    <CardContent>
+      <CardContentItem icon="portrait" text="Student" />
+      <CardContentItem icon="location_city" text="NJIT" />
+      <CardContentItem icon="school" text="Information Systems" />
+      <CardContentItem icon="email" text="ranr1141@njit.edu" />
+    </CardContent>
+  </CardCustom>;
+
+export const Ratings = () =>
+  <CardCustom>
+    <CardHeaderCustom name="Randal RiveraAAAAAA" rating={4.5} />
     <CardContent>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam fuga
       commodi eligendi a distinctio dicta quaerat quis explicabo reprehenderit,
