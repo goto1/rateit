e04d3a8d54a5b90723cd23c2e45dfef58551c592
Tomasz Goralczyk <devflag@gmail.com>
2017-07-01 12:29:48 -0400

    Added a new component that will display overview of user ratings

---
+++ src/components/RatingCategories.js
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import NumericRating from "./NumericRating";
+import HorizontalRule from "./HorizontalRule";
+import { Icon } from "framework7-react";
+
+const IconCustom = styled(Icon)`
+  flex-basis: 22px;
+  font-size: 19px;
+  padding-top: 4px;
+`;
+
+const Description = ({ description }) => {
+  const maxLength = 28;
+  return (
+    <div style={{ flexGrow: "1", paddingTop: "4px", color: "#000" }}>
+      {description.length > maxLength
+        ? `${description.slice(0, maxLength - 2)}..`
+        : description}
+    </div>
+  );
+};
+
+Description.propTypes = {
+  description: PropTypes.string.isRequired
+};
+
+const Category = ({ description, rating }) =>
+  <div style={{ display: "flex", marginBottom: "1px" }}>
+    <IconCustom material="forward" />
+    <Description description={description} />
+    <NumericRating rating={rating} fontSize={14} />
+  </div>;
+
+Category.propTypes = {
+  description: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+const RatingCategories = ({ ratings }) => {
+  const length = ratings.length;
+
+  return (
+    <div style={{ marginTop: "7.5px" }}>
+      {ratings.map((category, index) =>
+        <div key={index}>
+          <Category
+            description={category.description}
+            rating={category.rating}
+          />
+          {index !== length - 1 &&
+            <HorizontalRule
+              margin="1px auto"
+              width="70%"
+              colorOne="#e5e5e5"
+              colorTwo="#0b7eff"
+            />}
+        </div>
+      )}
+    </div>
+  );
+};
+
+RatingCategories.propTypes = {
+  ratings: PropTypes.array.isRequired
+};
+
+export default RatingCategories;
