75ab6cd693566fb6c085c05563f5955fa5d7ad73
Tomasz Goralczyk <devflag@gmail.com>
2017-07-04 13:39:09 -0400

    Refactored the ContentBlockTitle component and changed its name to better represent its purpose

---
+++ src/components/f7/ContentBlockTitleWrapper.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { ContentBlockTitle } from "framework7-react";
+
+const ContentBlockTitleWrapper = styled(ContentBlockTitle)`
+  margin-top: 25px !important;
+`;
+
+export default ContentBlockTitleWrapper;
