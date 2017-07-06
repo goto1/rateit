5d31285633e9d6a01de04fb431ea1d8aecf50967
Tomasz Goralczyk <devflag@gmail.com>
2017-07-06 14:39:36 -0400

    Packaged all related components into a single file for better reusability

---
+++ src/components/UserItemContents.js
@@ -0,0 +1,127 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import RatingStars from "./RatingStars";
+import capitalize from "lodash/capitalize";
+
+const UserNameWrapper = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+  flex-grow: 1;
+  color: ${props => props.color};
+`;
+
+const UserName = ({ type, maxLength, children }) => {
+  const color = type.toLowerCase() === "student" ? "#497B3C" : "#6967AE";
+  const username =
+    children.length > maxLength
+      ? `${children.slice(0, maxLength - 2)}..`
+      : children;
+
+  return (
+    <UserNameWrapper className="username" color={color}>
+      {username}
+    </UserNameWrapper>
+  );
+};
+
+UserName.propTypes = {
+  type: PropTypes.string.isRequired,
+  maxLength: PropTypes.number.isRequired,
+  children: PropTypes.string
+};
+
+const UserRating = styled.div`
+  border-radius: 5px;
+  background-color: #0b7eff;
+  color: #fff;
+  text-align: center;
+  font-weight: 300;
+  font-size: 15px;
+  width: 32.5px;
+  padding: 1px;
+  margin: ${props => props.margin || "0"};
+`;
+
+UserRating.propTypes = {
+  children: PropTypes.number.isRequired
+};
+
+const UserRoleWrapper = styled.div`
+  color: rgb(0, 0, 0);
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: .5px;
+`;
+
+const UserRole = ({ type, schools, maxLength }) => {
+  const schoolNames = schools.map(school => school.abbreviation).join(", ");
+
+  return (
+    <UserRoleWrapper>
+      {capitalize(type)} at{" "}
+      {schoolNames.length > maxLength
+        ? `${schoolNames.slice(0, maxLength - 2)}..`
+        : schoolNames}
+    </UserRoleWrapper>
+  );
+};
+
+const TotalRatingsWrapper = styled.div`
+  color: #8e8e93;
+  font-size: 14px;
+  flex-grow: 1;
+  margin-top: 2px;
+  span {
+    font-weight: 600;
+  }
+`;
+
+const TotalRatings = ({ numOfRatings }) =>
+  <TotalRatingsWrapper>
+    <span>{numOfRatings}</span> ratings
+  </TotalRatingsWrapper>;
+
+const Row = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const UserItemContentsContainer = styled.div`width: 100%;`;
+
+const UserItemContents = ({
+  name,
+  type,
+  schools,
+  overallRating,
+  userRatings,
+  margin
+}) =>
+  <UserItemContentsContainer>
+    <Row>
+      <UserName type={type} maxLength={20}>
+        {name}
+      </UserName>
+      <UserRating margin={margin}>
+        {overallRating}
+      </UserRating>
+    </Row>
+    <Row>
+      <UserRole type={type} schools={schools} maxLength={10} />
+    </Row>
+    <Row>
+      <TotalRatings numOfRatings={userRatings.length} />
+      <RatingStars rating={overallRating} />
+    </Row>
+  </UserItemContentsContainer>;
+
+UserItemContents.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(["student", "professor"]),
+  schools: PropTypes.array.isRequired,
+  overallRating: PropTypes.number.isRequired,
+  userRatings: PropTypes.array.isRequired,
+  margin: PropTypes.string
+};
+
+export default UserItemContents;
