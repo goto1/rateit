569dacc858c792b622a559b4f7959edd104279d5
Tomasz Goralczyk <devflag@gmail.com>
2017-07-06 11:15:51 -0400

    Created a new component for handling link list items

--- src/components/f7/index.js
+++ src/components/f7/index.js
@@ -1,17 +1,23 @@
 import ListItemCustom from "./ListItemCustom";
 import ListBlockCustom from "./ListBlockCustom";
 import ContentBlockTitleCustom from "./ContentBlockTitleCustom";
+import ContentBlockTitleWrapper from "./ContentBlockTitleWrapper";
 import FormInputCustom from "./FormInputCustom";
 import SmartSelectCustom from "./SmartSelectCustom";
 import SubmitButtonCustom from "./SubmitButtonCustom";
 import TextAreaCustom from "./TextAreaCustom";
 
+import LinkItemContainer from "./LinkItemContainer";
+
+// TODO: delete `ContentBlockTitleCustom` after refactoring
 export {
   ListItemCustom,
   ListBlockCustom,
   ContentBlockTitleCustom,
+  ContentBlockTitleWrapper,
   FormInputCustom,
   TextAreaCustom,
   SmartSelectCustom,
-  SubmitButtonCustom
+  SubmitButtonCustom,
+  LinkItemContainer
 };
