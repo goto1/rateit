3035dff365667337c4f3852c11dc2fc19fadb076
Tomasz Goralczyk <devflag@gmail.com>
2017-07-07 11:38:45 -0400

    Refactored the UserProfile container and change the name for more clarity

---
+++ src/containers/UserProfile.js
@@ -0,0 +1,48 @@
+import React from "react";
+import capitalize from "lodash/capitalize";
+import {
+  Page,
+  Navbar,
+  Subnavbar,
+  ButtonsSegmented,
+  Button,
+  Tab,
+  Tabs
+} from "framework7-react";
+
+// DELETE WHEN DONE TESTING
+import * as API from "../utils";
+
+const user = API.getUserDetails("UArjrbxWHX");
+
+console.log(user);
+
+const UserProfile = () => {
+  const title = `${capitalize(user.type)} Details`;
+  const overviewUrl = `/profile/${user.id}`;
+  const ratingsUrl = `/profile/${user.id}/ratings`;
+
+  return (
+    <Page withSubnavbar>
+      <Navbar title={title} backLink="Back" sliding>
+        <Subnavbar sliding>
+          <ButtonsSegmented>
+            <Button routeTabLink="#overview" href={overviewUrl}>
+              Overview
+            </Button>
+            <Button routeTabLink="#ratings" href={ratingsUrl}>
+              Ratings
+            </Button>
+          </ButtonsSegmented>
+        </Subnavbar>
+      </Navbar>
+
+      <Tabs>
+        <Tab routeTabId="overview" />
+        <Tab routeTabId="ratings" />
+      </Tabs>
+    </Page>
+  );
+};
+
+export default UserProfile;
