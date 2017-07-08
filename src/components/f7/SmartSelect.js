40d105f02827a642a20ef18beb9e1332bf06dbba
Tomasz Goralczyk <devflag@gmail.com>
2017-07-08 16:14:04 -0400

    Deleted unnecessary code

--- src/components/f7/SmartSelect.js
+++ src/components/f7/SmartSelect.js
@@ -42,47 +42,4 @@ SmartSelect.propTypes = {
   onChange: PropTypes.func.isRequired
 };
 
-// // <SmartSelectCustom
-// //   name="name"
-// //   options={[{}, {}]}
-// //   selected={['john', 'jeff']}
-// //   searchbarPlaceholder="Search names"
-// //   onChange={event => console.log(event)}
-// // />
-// const SmartSelectCustom = ({
-//   name,
-//   options,
-//   selected,
-//   searchbarPlaceholder,
-//   onChange
-// }) =>
-//   <li>
-//     <a
-//       href="#"
-//       className="item-link smart-select"
-//       data-searchbar="true"
-//       data-searchbar-placeholder={searchbarPlaceholder}
-//     >
-//       <select value={selected} name={name} onChange={onChange} multiple>
-//         {options.map(option =>
-//           <option key={option.id} value={option.id}>{option.name}</option>
-//         )}
-//       </select>
-//       <div className="item-content">
-//         <div className="item-inner">
-//           <div className="item-title">{capitalize(name)}</div>
-//           <div className="item-after" />
-//         </div>
-//       </div>
-//     </a>
-//   </li>;
-
-// SmartSelectCustom.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   options: PropTypes.array.isRequired,
-//   selected: PropTypes.array.isRequired,
-//   searchbarPlaceholder: PropTypes.string.isRequired,
-//   onChange: PropTypes.func.isRequired
-// };
-
 export default SmartSelect;
