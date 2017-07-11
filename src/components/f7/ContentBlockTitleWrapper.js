30817b949c72847abb26abbf2dcd75f1dde452b5
Tomasz Goralczyk <devflag@gmail.com>
2017-07-11 11:10:41 -0400

    Removed old ContentBlockTitle component after refactoring

--- src/components/f7/ContentBlockTitleWrapper.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { ContentBlockTitle } from "framework7-react";
-
-const ContentBlockTitleWrapper = styled(ContentBlockTitle)`
-  margin-top: 25px !important;
-`;
-
-export default ContentBlockTitleWrapper;
