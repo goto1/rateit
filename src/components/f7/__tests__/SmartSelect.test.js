c4f8e824a64be81600c7398eb15f071f948457cb
Tomasz Goralczyk <devflag@gmail.com>
2017-07-12 09:47:37 -0400

    Created a unit test for the SmartSelect component

---
+++ src/components/f7/__tests__/SmartSelect.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import SmartSelect from "../SmartSelect";
+import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
+import "jest-enzyme";
+
+const props = {
+  name: "schools",
+  options: [
+    {
+      id: "bruHy",
+      name: "New Jersey Institute of Technology, Newark",
+      abbreviation: "NJIT"
+    },
+    {
+      id: "5y7fh",
+      name: "Rutgers University, New Brunswick",
+      abbreviation: "RU"
+    }
+  ],
+  selected: ["bruHy"],
+  searchbarPlaceholder: "Search for schools...",
+  onChange: jest.fn()
+};
+
+describe("SmartSelect", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<SmartSelect {...props} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("calls a function with selection changes", () => {
+    const wrapper = shallow(<SmartSelect {...props} />);
+    wrapper.find("select").simulate("change");
+
+    const onChange = props.onChange;
+
+    expect(onChange.mock.calls.length).toBe(1);
+  });
+});
