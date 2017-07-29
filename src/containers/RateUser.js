de7355bfaf6ad3893226307485c99c359cff7636
Tomasz Goralczyk <devflag@gmail.com>
2017-07-29 15:02:27 -0400

    Minor refactoring

--- src/containers/RateUser.js
+++ src/containers/RateUser.js
@@ -48,165 +48,141 @@ FormSection.propTypes = {
   children: PropTypes.node.isRequired
 };
 
-class StudentRatingForm extends Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log("submitted..");
-  };
+// DELETE WHEN DONE TESTING
+const user = API.getUserDetails("UArjrbxWHX");
+const schools = user.schools;
+const majors = API.getMajors();
+const selectedSchools = schools.slice(0, 1).map(school => school.id);
+const selectedMajors = user.majors.slice(0, 1).map(school => school.id);
+const ratingCategoriesStud = API.getRatingCategories().student;
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    console.log(name, value);
-  };
+const StudentRatingForm = ({ handleSubmit, handleChange }) =>
+  <form onSubmit={handleSubmit}>
+    <FormSection title="Student Information">
+      <InputElement
+        icon="account_circle"
+        name="student_name"
+        type="text"
+        placeholder="John Doe"
+        onChange={handleChange}
+      />
+      <InputElement
+        icon="email"
+        name="student_email"
+        type="email"
+        placeholder="john@school.edu"
+        onChange={handleChange}
+      />
+    </FormSection>
+    <FormSection title="School Information">
+      <SmartSelect
+        name="school"
+        options={schools}
+        selected={selectedSchools}
+        multiple={false}
+        searchbarPlaceholder="Search for a school..."
+        onChange={handleChange}
+      />
+      <SmartSelect
+        name="major"
+        options={majors}
+        selected={selectedMajors}
+        multiple={false}
+        searchbarPlaceholder="Search for a major..."
+        onChange={handleChange}
+      />
+    </FormSection>
+    {ratingCategoriesStud.map(category => {
+      const props = {
+        ...category,
+        key: category.id,
+        onChange: handleChange
+      };
+      return <RatingCategoryInput {...props} />;
+    })}
+    <FormSection title="Additional Information">
+      <Textarea
+        icon="comment"
+        name="comment"
+        placeholder="Your comment here..."
+        onChange={handleChange}
+      />
+      <RadioInput icon="done" name="recommend" onChange={handleChange} />
+    </FormSection>
+    <ContentBlock>
+      <Button type="submit" color="green" big fill>
+        Submit
+      </Button>
+    </ContentBlock>
+  </form>;
 
-  render() {
-    // DELETE WHEN DONE TESTING
-    const user = API.getUserDetails("UArjrbxWHX");
-    const schools = user.schools;
-    const majors = API.getMajors();
-    const selectedSchools = schools.slice(0, 1).map(school => school.id);
-    const selectedMajors = user.majors.slice(0, 1).map(school => school.id);
-    const ratingCategories = API.getRatingCategories().student;
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <FormSection title="Student Information">
-          <InputElement
-            icon="account_circle"
-            name="student_name"
-            type="text"
-            placeholder="John Doe"
-            onChange={this.handleChange}
-          />
-          <InputElement
-            icon="email"
-            name="student_email"
-            type="email"
-            placeholder="john@school.edu"
-            onChange={this.handleChange}
-          />
-        </FormSection>
-        <FormSection title="School Information">
-          <SmartSelect
-            name="school"
-            options={schools}
-            selected={selectedSchools}
-            searchbarPlaceholder="Search for a school..."
-            onChange={this.handleChange}
-          />
-          <SmartSelect
-            name="major"
-            options={majors}
-            selected={selectedMajors}
-            searchbarPlaceholder="Search for a major..."
-            onChange={this.handleChange}
-          />
-        </FormSection>
-        {ratingCategories.map(category => {
-          const props = {
-            ...category,
-            key: category.id,
-            onChange: this.handleChange
-          };
-          return <RatingCategoryInput {...props} />;
-        })}
-        <FormSection title="Additional Information">
-          <Textarea
-            icon="comment"
-            name="comment"
-            placeholder="Your comment here..."
-            onChange={this.handleChange}
-          />
-          <RadioInput
-            icon="done"
-            name="recommend"
-            onChange={this.handleChange}
-          />
-        </FormSection>
-        <ContentBlock>
-          <Button type="submit" color="green" big fill>
-            Submit
-          </Button>
-        </ContentBlock>
-      </form>
-    );
-  }
-}
+const ratingCategoriesProf = API.getRatingCategories().professor;
 
-class ProfessorRatingForm extends Component {
-  handleSubmit = e => {
-    e.preventDefault();
-    console.log("submitted..");
-  };
+const ProfessorRatingForm = ({ handleSubmit, handleChange }) =>
+  <form onSubmit={handleSubmit}>
+    <FormSection title="Professor Information">
+      <InputElement
+        icon="account_circle"
+        name="professor_name"
+        type="text"
+        placeholder="John Doe"
+        onChange={handleChange}
+      />
+    </FormSection>
+    <FormSection title="School Information">
+      <SmartSelect
+        name="school"
+        options={schools}
+        selected={selectedSchools}
+        multiple={false}
+        searchbarPlaceholder="Search for a school..."
+        onChange={handleChange}
+      />
+      <SmartSelect
+        name="major"
+        options={majors}
+        selected={selectedMajors}
+        multiple={false}
+        searchbarPlaceholder="Search for a major..."
+        onChange={handleChange}
+      />
+    </FormSection>
+    {ratingCategoriesProf.map(category => {
+      const props = {
+        ...category,
+        key: category.id,
+        onChange: handleChange
+      };
+      return <RatingCategoryInput {...props} />;
+    })}
+    <ContentBlock>
+      <Button type="submit" color="green" big fill>
+        Submit
+      </Button>
+    </ContentBlock>
+  </form>;
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    console.log(name, value);
-  };
+const handleSubmit = e => {
+  e.preventDefault();
+  console.log("submitted...");
+};
 
-  render() {
-    // DELETE WHEN DONE TESTING //
-    const user = API.getUserDetails("UArjrbxWHX");
-    const schools = user.schools;
-    const majors = API.getMajors();
-    const selectedSchools = schools.slice(0, 1).map(school => school.id);
-    const selectedMajors = user.majors.slice(0, 1).map(school => school.id);
-    const ratingCategories = API.getRatingCategories().professor;
-    // DELETE WHEN DONE TESTING //
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <FormSection title="Professor Information">
-          <InputElement
-            icon="account_circle"
-            name="professor_name"
-            type="text"
-            placeholder="John Doe"
-            onChange={() => "onChange"}
-          />
-        </FormSection>
-        <FormSection title="School Information">
-          <SmartSelect
-            name="school"
-            options={schools}
-            selected={selectedSchools}
-            searchbarPlaceholder="Search for a school..."
-            onChange={() => "onChange"}
-          />
-          <SmartSelect
-            name="major"
-            options={majors}
-            selected={selectedMajors}
-            searchbarPlaceholder="Search for a major..."
-            onChange={() => "onChange"}
-          />
-        </FormSection>
-        {ratingCategories.map(category => {
-          const props = {
-            ...category,
-            key: category.id,
-            onChange: this.handleChange
-          };
-          return <RatingCategoryInput {...props} />;
-        })}
-        <ContentBlock>
-          <Button type="submit" color="green" big fill>
-            Submit
-          </Button>
-        </ContentBlock>
-      </form>
-    );
-  }
-}
+const handleChange = e => {
+  const { name, value } = e.target;
+  console.log(`${name}: ${value}`);
+};
 
 let RateUser = ({ currentRoute }) => {
   const userType = JSON.parse(currentRoute.query).type;
+  const props = { handleSubmit, handleChange };
   return (
     <Page>
       <Navbar>
         <NavLeft backLink="Cancel" sliding />
         <NavCenter sliding>Rating Submission</NavCenter>
       </Navbar>
-      {userType === "professor" ? <ProfessorRatingForm /> : null}
-      {userType === "student" ? <StudentRatingForm /> : null}
+      {userType === "professor" ? <ProfessorRatingForm {...props} /> : null}
+      {userType === "student" ? <StudentRatingForm {...props} /> : null}
     </Page>
   );
 };
