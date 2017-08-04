2a5da3069e4acb45e4e978ad562535dc2c198cc1
Tomasz Goralczyk <devflag@gmail.com>
2017-08-04 17:50:55 -0400

    Implemented formik and yup with the Help component

--- src/containers/Help.js
+++ src/containers/Help.js
@@ -1,38 +1,88 @@
 import React from "react";
+import { Formik } from "formik";
+import Yup from "yup";
 import {
   Button,
   ContentBlock,
   ContentBlockTitle,
   InputElement,
   List,
+  NavCenter,
+  NavLeft,
+  Navbar,
   Textarea
 } from "../components/f7";
-import { Page, Navbar } from "framework7-react";
+import { Page } from "framework7-react";
+
+let Form = ({
+  values,
+  errors,
+  touched,
+  handleChange,
+  handleBlur,
+  handleSubmit,
+  isSubmitting
+}) => {
+  const invalidForm = Object.keys(errors).length !== 0;
+  return (
+    <form onSubmit={handleSubmit}>
+      <List inset>
+        <InputElement
+          icon="info"
+          type="text"
+          name="title"
+          value={values.title}
+          placeholder="Title..."
+          onChange={handleChange}
+          onBlur={handleBlur}
+          valid={errors.title && touched.title && false}
+        />
+        <Textarea
+          icon="comment"
+          name="message"
+          value={values.message}
+          placeholder="Your message here..."
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+      </List>
+      <ContentBlock>
+        <Button
+          type="submit"
+          color="green"
+          disabled={isSubmitting || invalidForm}
+          big
+          fill
+        >
+          Submit
+        </Button>
+      </ContentBlock>
+    </form>
+  );
+};
+
+Form = Formik({
+  mapPropsToValues: props => ({
+    title: "",
+    message: ""
+  }),
+  validationSchema: Yup.object().shape({
+    title: Yup.string().required(),
+    message: Yup.string().required()
+  }),
+  handleSubmit: (values, { props, setErrors, setSubmitting }) => {
+    console.log(values);
+  }
+})(Form);
 
 const Help = () =>
   <Page>
-    <Navbar title="Help Center" backLink="Back" sliding />
+    <Navbar>
+      <NavLeft backLink="Back" sliding />
+      <NavCenter sliding>Help Center</NavCenter>
+    </Navbar>
     <ContentBlockTitle>Contact form</ContentBlockTitle>
-    <List inset={true}>
-      <InputElement
-        icon="info"
-        type="text"
-        name="title"
-        placeholder="Title..."
-        onChange={e => console.log(e.target.name, e.target.value)}
-      />
-      <Textarea
-        icon="comment"
-        name="message"
-        placeholder="Your message here..."
-        onChange={e => console.log(e.target.name, e.target.value)}
-      />
-    </List>
-    <ContentBlock>
-      <Button color="green" onClick={() => "onClick"} big fill>
-        Submit
-      </Button>
-    </ContentBlock>
+    <Form />
   </Page>;
 
 export default Help;
