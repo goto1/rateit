2afd6e79145316ce6f868d0da4ad17468eb7e3c0
Tomasz Goralczyk <devflag@gmail.com>
2017-08-18 17:29:54 -0400

    created a new set of utility functions to better represent the API calls that will be made to a backend service

---
+++ src/utils/API.js
@@ -0,0 +1,82 @@
+import {
+  schools,
+  majors,
+  ratingCategories,
+  userRatings,
+  users
+} from "../dummy-data";
+
+const getUserDetails = userId => {
+  const user = users.find(user => user.id === userId);
+  const clone = { ...user };
+
+  clone.schools = getSchoolDetails(user.schools);
+  clone.majors = getMajorDetails(user.majors);
+  clone.userRatings = getUserRatings(user.id);
+
+  if (clone.type === "student") {
+    clone.aggregateRatings = getStudentAggrRatingDetails(
+      clone.aggregateRatings
+    );
+    clone.userRatings = clone.userRatings.map(rating => ({
+      ...rating,
+      individualRatings: getStudentAggrRatingDetails(rating.individualRatings)
+    }));
+  } else {
+    clone.aggregateRatings = getProfessorAggrRatingDetails(
+      clone.aggregateRatings
+    );
+    clone.userRatings = clone.userRatings.map(rating => ({
+      ...rating,
+      individualRatings: getProfessorAggrRatingDetails(rating.individualRatings)
+    }));
+  }
+
+  return clone;
+};
+
+const getSchoolDetails = schoolIds =>
+  schoolIds.map(schoolId => schools.find(school => school.id === schoolId));
+
+const getMajorDetails = majorIds =>
+  majorIds.map(majorId => majors.find(major => major.id === majorId));
+
+const getUserRatings = userId =>
+  userRatings.filter(rating => rating.userId === userId);
+
+const getStudentAggrRatingDetails = aggrRatings =>
+  aggrRatings.map(rating => {
+    const details = ratingCategories.student.find(
+      category => category.id === rating.id
+    );
+    return { description: details.description, rating: rating.rating };
+  });
+
+const getProfessorAggrRatingDetails = aggrRatings =>
+  aggrRatings.map(rating => {
+    const details = ratingCategories.professor.find(
+      category => category.id === rating.id
+    );
+    return { description: details.description, rating: rating.rating };
+  });
+
+export const fetchUser = id =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!id) {
+        reject({
+          error: {
+            code: 404,
+            message: "User ID is not found"
+          }
+        });
+      } else {
+        const user = getUserDetails(id);
+        resolve({
+          data: {
+            ...user
+          }
+        });
+      }
+    }, 1000);
+  });
